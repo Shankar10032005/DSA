e7a9e0cade8a7039f575776f1b6b8075aa31c438
SHANKAR  I <shankarilango2005@gmail.com>
2024-12-26 23:29:24 +0530

    Create solution.py

---
+++ Basics/solution.py
@@ -0,0 +1,16 @@
+class Solution {
+    static int dataTypeSize(String str) {
+        if (str.equals("Character")){
+            return 1;
+        }   
+        else if (str.equals("Integer")){
+            return 4;
+        }
+        else if (str.equals("Float")){
+            return 4;
+        }
+        else {
+            return 8;
+        }
+        }
+}
