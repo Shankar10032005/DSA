ef7ae56ff03c77fdf6620e98b890659366457c54
SHANKAR  I <shankarilango2005@gmail.com>
2024-12-27 00:03:13 +0530

    Create Solution.java

---
+++ Basics/Pass by Reference and Value/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        a=a+1;
+        b=b+2;
+        
+        int[] arr=new int[2];
+        arr[0]=a;
+        arr[1]=b;
+        
+        return arr;
+    }
+}
