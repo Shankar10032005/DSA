2968759fb809424895f46017eea8d21678d82547
SHANKAR  I <shankarilango2005@gmail.com>
2024-12-27 00:00:21 +0530

    Create Solution.java

---
+++ Basics/While loop- printTable - Java/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=10;
+        while(k>0){
+            System.out.print(n*k+" ");
+            k -=1;
+            
+        }
+    }
+}
