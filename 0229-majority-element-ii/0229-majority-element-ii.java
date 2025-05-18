059b10a9bf057da39e01d9282ba958fd3b7b0d46
SHANKAR  I <shankarilango2005@gmail.com>
2025-05-19 00:19:54 +0530

    Time: 3 ms (73.02%), Space: 50.3 MB (14.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,60 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int len=(nums.length/3)+1;
+        
+        int num1=Integer.MIN_VALUE;
+        int num2=Integer.MIN_VALUE;
+        int count1=0;
+        int count2=0;
+        int i=0;
 
-        for(int i=0;i<=nums.length-1;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])>=len  && !list.contains(nums[i])){
-                list.add(nums[i]);
+        while(i<=nums.length-1){
+
+            if(count1 ==0 && num2 != nums[i]){
+            num1=nums[i];
+            count1 = 1;
+           
+
             }
-            if(list.size()==2 ){
-                break;
+            else if(count2 == 0 && num1 != nums[i]){
+            num2=nums[i];
+            count2=1;
+            
+            }
+            else if(nums[i]==num1){
+                count1+=1;
+            }
+            else if(nums[i]==num2){
+                count2+=1;
+            }
+            else{
+                count1-=1;
+                count2-=1;
+                
+            }
+            i++;
+
+
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        int len=(nums.length/3)+1;
+        int cnt1=0;
+        int cnt2=0;
+        for(int j=0;j<=nums.length-1;j++){
+            if(nums[j]==num1){
+                cnt1+=1;
+                if(cnt1==len){
+                    list.add(num1);
+                }
+            }
+            else if(nums[j]==num2){
+                cnt2+=1;
+                if(cnt2==len){
+                    list.add(num2);
+                }
             }
         }
+
         return list;
+        
     }
 }
