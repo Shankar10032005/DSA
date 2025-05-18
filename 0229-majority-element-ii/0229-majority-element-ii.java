05d4a492d03855e6c88ea42fac891a980bc5e22b
SHANKAR  I <shankarilango2005@gmail.com>
2025-05-18 16:18:48 +0530

    Time: 18 ms (7.08%), Space: 50.1 MB (15.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -6,10 +6,10 @@ class Solution {
 
         for(int i=0;i<=nums.length-1;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])==len){
+            if(map.get(nums[i])==len  && !list.contains(nums[i])){
                 list.add(nums[i]);
             }
-            if(list.size()==2){
+            if(list.size()==2 ){
                 break;
             }
         }
