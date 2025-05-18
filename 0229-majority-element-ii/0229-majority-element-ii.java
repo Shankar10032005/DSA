3dc9a47918612a28c14b02ebde73823fa2c85a0d
SHANKAR  I <shankarilango2005@gmail.com>
2025-05-18 16:14:35 +0530

    Time: 18 ms (7.08%), Space: 49.5 MB (18.92%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,17 +3,14 @@ class Solution {
         ArrayList<Integer> list=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         HashSet<Integer> seen = new HashSet<>();
-        int len=nums.length/3;
+        int len=(nums.length/3)+1;
 
         for(int i=0;i<=nums.length-1;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-          for(int i=0;i<=nums.length-1;i++){
-            if(map.get(nums[i]) > len && !seen.contains(nums[i])){
+            if(map.get(nums[i])==len){
                 list.add(nums[i]);
-                seen.add(nums[i]);
             }
-          }
-          return list;
+        }
+        return list;
     }
 }
