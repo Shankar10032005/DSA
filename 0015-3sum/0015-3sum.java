9c745bd98542c3f68152cfc6ec0f6baff497fdcf
SHANKAR  I <shankarilango2005@gmail.com>
2025-05-19 13:49:13 +0530

    Time: 30 ms (69.17%), Space: 51.4 MB (71.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> list=new HashSet<>();
         Arrays.sort(nums);
-
-    
-    int a=0;
-     while(a<=nums.length-3){
-        int b=a+1;
-        int c=nums.length-1;
-
-        while(b<c){
-            if(nums[a]+nums[b]+nums[c]<0){
-                b++;
-            }
-            else if(nums[a]+nums[b]+nums[c]>0){
-                c--;
-            }
-            else{
-                List<Integer> triplet=Arrays.asList(nums[a],nums[b],nums[c]);
-                list.add(triplet);
-                b++;
-                c--;
-                while (b < c && nums[b] == nums[b - 1]) {
+        List<List<Integer>> list=new ArrayList<>();
+        int a=0;
+        while(a<=nums.length-3){
+            int b=a+1;
+            int c=nums.length-1;
+            while(b<c){
+                if(nums[a]+nums[b]+nums[c] < 0){
+                    b++;
+                    
+                }
+                
+                else if(nums[a]+nums[b]+nums[c] > 0){
+                    c--;
+                  
+                }
+                else{
+                    List<Integer> triplet=Arrays.asList(nums[a],nums[b],nums[c]);
+                    list.add(triplet);
+                    b++;
+                    c--;      
+                    while(b<c && nums[b]==nums[b-1]){
                         b++;
-                 }
-                while (b < c && nums[c] == nums[c + 1]) {
+                    }    
+                    while(b<c && nums[c]==nums[c+1]){
                         c--;
-                    }
+                    }                              
+                }
             }
+            a=a+1;
+            while(a<=nums.length-3 && nums[a]==nums[a-1]){
+                        a++;
+            }
+
+
         }
-        a+=1;
-        while (a <= nums.length - 3 && nums[a] == nums[a - 1]) {
-                a++;
-            }        
-    }  
-    return new ArrayList<>(list);
-}
+        return new ArrayList<>(list);
+        
+    }
 }
